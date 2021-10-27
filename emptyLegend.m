3afaeaedab685349e937a095d8029c6df76e5420
John Grogan <groganj1@tcd.ie>
2021-10-27 16:26:22 +0100

    changed how empty legend works - takes multiple cells per line now, plus cell for all lines

--- emptyLegend.m
+++ emptyLegend.m
@@ -1,23 +1,29 @@
-function h = emptyLegend(n, opts, cols, names, legOpts)
+function h = emptyLegend(n, opts, allOpts, names, legOpts)
 % function h = emptyLegend(n, opts, cols, names)
 % plots NaN so that you have plot handles for an empty plot which can be
 % used for legends
 % 
 % Inputs:
 %   n = number of lines/plots to make
-%   opts = [N x 1] cell array of strings to pass to plot e.g. '-x'
-%   cols = [N x 1] cell array of colour args to pass to plot(NaN,'Color'...)
+%   opts = nx1 cell array, each containing one cell of arguments per line.
+%   allOpts = cell array of options to be passed to all lines 
 %   names = cell array of legend names
 %   legOpts = cell array of options passed to legend
+% 
+% Example:
+%   emptyLegend(2, {{'-r','LineWidth',2}, {'--','Color',[1 .2 .3]}}, {'Marker','x'}, {'test','control'}, {'Location','Best'});
 
 
     if ~exist('legOpts','var') || isempty(legOpts)
         legOpts = {};
     end
+    if ~exist('allOpts','var') || isempty(allOpts)
+        allOpts = {};
+    end
     
     % plot each set of options as NaN
     for i = 1:n
-        h(i) = plot(NaN, opts{i}, 'Color', cols{i}, 'LineWidth',2, 'MarkerFaceColor', cols{i});
+        h(i) = plot(NaN, opts{i}{:}, allOpts{:});
         hold on;
     end
     
