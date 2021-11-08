354373449cfcfd82a1c34f9adba304c8270c671c
John Grogan <groganj1@tcd.ie>
2021-11-08 11:19:00 +0000

    pull out coeff values from cell array of lme/glme tables

---
+++ StatsTableFromLMECells.m
@@ -0,0 +1,67 @@
+function statTable = StatsTableFromLMECells(lmeCells, colNames, statName, keepIntercept)
+% function statTable = StatsTableFromLMECells(lmeCells, colNames, statName, keepIntercept)
+% extract coefficient values from a cell vector of LME/GLME outputs
+% store as a table
+% Inputs:
+%   lmeCells = cell vector of LME/GLME outputs. will ignore empty cells
+%   colNames = cell array of strings. names to give columns of table created. 
+%   statName = name of statistic to get from Coefficients. default is 'pValue'
+%   keepIntercept = whether to keep the intercept or not (default = 0)
+% 
+% Outputs:
+%   stats = table with row per term and column per cell
+% 
+
+%% check inputs
+
+if ~iscell(lmeCells) || ~isvector(lmeCells)
+    error('lmeCells must be a cell array of LME/GLME outputs'); 
+end
+
+if exist('colNames','var') && ~isempty(colNames)
+    if numel(colNames) ~= numel(lmeCells) || ~isvector(colNames)
+        error('colNames size does not match lmeCells size');
+    end
+else
+    colNames = [];
+end
+
+if ~exist('statName','var') || isempty(statName)
+    statName = 'pValue';
+end
+
+if ~exist('keepIntercept','var') || isempty(keepIntercept)
+    keepIntercept = 0;
+end
+
+
+
+% remove empties
+emptyCells = cellfun(@isempty, lmeCells);
+lmeCells(emptyCells) = [];
+colNames(emptyCells) = [];
+
+% check all are LMe/GLME type
+if any( ~cellfun(@(x) isa(x, 'LinearMixedModel') || isa(x, 'GeneralizedLinearMixedModel'), lmeCells))
+    error('not all filled cells are LME/GLME types');
+end
+
+
+%% get stat
+
+stat = zeros(length(lmeCells{1}.CoefficientNames), length(lmeCells));
+
+for i = 1:length(lmeCells)
+    stat(:,i) = lmeCells{i}.Coefficients.(statName);
+end
+
+% convert to table
+statTable = array2table(stat,'RowNames',lmeCells{1}.CoefficientNames);
+
+if ~isempty(colNames)
+    statTable.Properties.VariableNames = colNames;
+end
+
+if ~keepIntercept
+    statTable(1,:) = [];
+end    
