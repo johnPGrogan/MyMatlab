8cef830d3a9c750962c08a29e348dac8fc32455b
John Grogan <groganj1@tcd.ie>
2022-03-03 16:49:44 +0000

    can do bar plots now

--- emptyLegend.m
+++ emptyLegend.m
@@ -1,5 +1,5 @@
-function h = emptyLegend(n, opts, allOpts, names, legOpts)
-% function h = emptyLegend(n, opts, cols, names)
+function h = emptyLegend(n, opts, allOpts, names, legOpts, plotfun)
+% function h = emptyLegend(n, opts, cols, names, plotfun)
 % plots NaN so that you have plot handles for an empty plot which can be
 % used for legends
 % 
@@ -9,6 +9,7 @@ function h = emptyLegend(n, opts, allOpts, names, legOpts)
 %   allOpts = cell array of options to be passed to all lines 
 %   names = cell array of legend names
 %   legOpts = cell array of options passed to legend
+%   plotfun = plotting function handle (default is @plot)
 % 
 % Example:
 %   emptyLegend(2, {{'-r','LineWidth',2}, {'--','Color',[1 .2 .3]}}, {'Marker','x'}, {'test','control'}, {'Location','Best'});
@@ -20,10 +21,13 @@ function h = emptyLegend(n, opts, allOpts, names, legOpts)
     if ~exist('allOpts','var') || isempty(allOpts)
         allOpts = {};
     end
+    if ~exist('plotfun','var') || isempty(plotfun)
+        plotfun = @plot;
+    end
     
     % plot each set of options as NaN
     for i = 1:n
-        h(i) = plot(NaN, opts{i}{:}, allOpts{:});
+        h(i) = plotfun(NaN, opts{i}{:}, allOpts{:});
         hold on;
     end
     
