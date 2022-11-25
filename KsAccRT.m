ba40f33917e651cf878b3bcb350920e6d1b5b44a
John Grogan <groganj1@tcd.ie>
2022-11-25 13:18:53 +0000

    normalise by mean of these, not sum

--- KsAccRT.m
+++ KsAccRT.m
@@ -4,7 +4,7 @@ function h = KsAccRT(rt, acc, invertErrors, normaliseHeight, fillColours, ksArgs
 % 
 % Inputs:
 %   rt  = [n x 1] vector of RTs
-%   acc = [n x 1] vector of accuracies (1/0)
+%   acc = [n x 1] vector of accuracies (1/0/NaN) - remove any ==2
 %   invertErrors = 1=errors are mirrored in y-axis, 0=errors and correct both above x-axis
 %   normaliseHeight = 1=normalise pdf of error/correct to both peak at 1, 
 %     2=normalise by proportion of corr/err (e.g. 75% acc will mean corr is 3x higher)
@@ -12,7 +12,8 @@ function h = KsAccRT(rt, acc, invertErrors, normaliseHeight, fillColours, ksArgs
 %   fillColours = 0=just line plot (uses colour order), 1=fill using default colours, 
 %     or can give [2x3] rbg colours [correct; error] or cell {2 x 1} of
 %     colour letters. fill uses 'hold on' to do error/correct separately
-%   ksArgs = cell array that is passed into ksdensity as ksArgs{:}
+%   ksArgs = cell array that is passed into ksdensity as ksArgs{:}, can control
+%     the x-axis limits via XI or Support (see help ksdensity) as well as bandwidth etc
 %   plotArgs = cell array passed to plot/fill call. You can control the colour order
 %     by using set(gca,'ColorOrder'...), if easier. Colour is controlled
 %     separately for fill (fillColours argument), but transparency
@@ -58,7 +59,7 @@ if normaliseHeight==1 % make it peak at 1
     y = (y ./ max(y,[],2));
 
 elseif normaliseHeight==2 % scale by p(correct)
-    y = (y ./ max(y,[],2)) .* sum(acc == [1 0])';
+    y = (y ./ max(y,[],2)) .* mean(acc == [1 0])';
 end
 
 
