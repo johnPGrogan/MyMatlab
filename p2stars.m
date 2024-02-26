2b7d450df275c6749b5db0cb60e71123458e0143
John Grogan <john.grogan@tcd.ie>
2024-02-26 11:47:49 +0000

    can swap symbols if wanted

--- p2stars.m
+++ p2stars.m
@@ -1,4 +1,5 @@
-function stars = p2stars(p, criteria)
+function stars = p2stars(p, criteria, symbol)
+% function stars = p2stars(p, criteria, symbol)
 % converts p values into cell array of strings of stars. default criteria =
 % * = p < .05
 % ** = p < .01
@@ -8,6 +9,7 @@ function stars = p2stars(p, criteria)
 % inputs: 
 %   p = matrix of p values, any size
 %   criteria = vector of decreasing p value criteria. default is [.05, .01, .001, .0001]
+%   symbols = symbol to return, default is '*'
 % 
 % outputs:
 %   stars = cell array matching size of p, with one '*' per criteria passed
@@ -17,9 +19,14 @@ function stars = p2stars(p, criteria)
 if ~exist('criteria','var') || isempty(criteria)
     criteria = [.05, .01, .001, .0001];
 end
+if ~exist('symbol','var') || isempty(symbol)
+    symbol = '*';
+else
+    assert(ischar(symbol) && length(symbol)==1, 'symbol must be 1 character')
+end
 
 stars = cell(size(p)); % preset
 
 for i = 1:length(criteria)
-    stars(p <= criteria(i)) = {repmat('*', 1, i)};
+    stars(p <= criteria(i)) = {repmat(symbol, 1, i)};
 end
