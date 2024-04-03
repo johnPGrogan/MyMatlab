c505b96f41b9ab1da61c4aaac948a103414185f1
John Grogan <john.grogan@tcd.ie>
2024-04-03 12:07:09 +0100

    pbar should be <= not < alpha

--- pbar.m
+++ pbar.m
@@ -5,7 +5,7 @@ function h = pbar(pVals, varargin)
 %         name-value pairs of optional inputs for graphical controls:
 %           xVals = x-axis values to plot to (same size as pVals)
 %           yVal = 0. y value to plot line along
-%           alpha = .05. alpha threshold, only values below are plotted
+%           alpha = .05. alpha threshold, only values <= this are plotted
 %           plotargs = cell array of name-par to pass to plot (e.g. Color
 %           or LineWidth)
 % 
@@ -16,7 +16,7 @@ names = {'yVal', 'alpha','plotargs','xVals'};
 defaults = {0, .05, {'Color','k','LineWidth',5}, 1:length(pVals)};
 [yVal, alpha, plotargs, xVals] = parsepvpairs(names, defaults, varargin{:});
 
-pVal2 = double(pVals < alpha); % get less than alpha
+pVal2 = double(pVals <= alpha); % get less than alpha
 pVal2(pVal2==0) = NaN; % set others to NaN
 
 h = plot(xVals, pVal2 .* yVal, '-', plotargs{:});
